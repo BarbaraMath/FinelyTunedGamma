eabe0b1932843b442307509504c5b209554a8968
BarbaraMath <mathiopoulou.barbara@gmail.com>
2023-01-24 17:00:03 +0100

    function documentation

--- baseline_correction.py
+++ baseline_correction.py
@@ -1,12 +1,29 @@
 ####### BASELINE CORRECTION #######
 
-def baseline_corr(raw, data, t, baseline, stim_ch, new_bcfname):
+def baseline_corr(data, t, baseline, raw = 0, stim_ch = 0):
+    """
+    baseline_corr performs a zlogratio baseline correction of the input recording
+
+    input:
+    - data: np.array of shape 1 x frequencies x time
+    - t: last dimension of the data (time)
+    - baseline: tuple with two numbers i.e. the baseline e.g. (1,100)
+    - raw: optional argument, when we want to ploit stimulation on top of recording
+    - stim_ch: optional argument, to specify stimulation channel e.g. 4(LSTN) or 5(RSTN)
+
+    """
     bs_data = mne.baseline.rescale(data = data, times = t, baseline = baseline, mode = 'zlogratio')
 
     fig, ax = plt.subplots(1,1,figsize = (7,7))
-    ax2 = ax.twinx()
-
     cf = ax.pcolormesh(bs_data, cmap = 'viridis', vmin = -1, vmax = 3)
+
+    if raw != 0:
+        ax2 = ax.twinx()
+        stim_data = (raw.get_data(picks = stim_ch)[0,:]/3)
+        ax2.plot(raw.times, stim_data,'w',linewidth = 1.5)
+        ax2.set_yticks(np.arange(0,4.5,0.5))
+        ax2.set_ylabel('Stimulation Amplitude [mA]')
+    
     
     ax.set_ylim(5, 100)
     ax.set_ylabel('Frequency [Hz]')
@@ -16,11 +33,6 @@ def baseline_corr(raw, data, t, baseline, stim_ch, new_bcfname):
     cbar = fig.colorbar(cf, ax = ax, location = 'bottom', pad = 0.1, shrink = 0.5)
     cbar.set_label('zlogratio')
 
-    stim_data = (raw.get_data(picks = stim_ch)[0,:]/3)
-    ax2.plot(raw.times, stim_data,'w',linewidth = 1.5)
-    ax2.set_yticks(np.arange(0,4.5,0.5))
-    ax2.set_ylabel('Stimulation Amplitude [mA]')
-    
     plt.show(block = False)
 
     #np.save(new_bcfname + '.npy', bs_data)
--- normalization.py
+++ normalization.py
@@ -1,8 +1,20 @@
-def normalization(Sxx):
-   
+####### NORMALIZATION #######
+
+def sum_normalization(Sxx):
+   """
+   sum_normalization performs a total sum normalization of the recording.
+   Preferably apply this only to recordings cleared of stimulation or other artefacts
+
+   input <-- Sxx:
+   np.array of shape channels x freqs x time e.g. data = raw.get_data(picks=[0,1]) 
+   data transformed to the time frequency domain
+
+   output <-- Sxx_norm
+   """
    fig, ax = plt.subplots(1,2, figsize = (18,6))
    fig.suptitle('Normalized Spectrogram')
    
+   #pre-allocate empty array 
    Sxx_norm = np.empty(shape = (Sxx.shape[0], Sxx.shape[1], Sxx.shape[2]))
    Sxx_norm[:] = np.nan
 
@@ -14,7 +26,6 @@ def normalization(Sxx):
       
       for j in range(Sxx.shape[2]):
          #A. Total Sum Normalization
-         
          Sxx_thisnorm = (Sxx[kl,:,j]/Sxx_totalsum)*100
          Sxx_chan[:,j] = Sxx_thisnorm
 
@@ -26,6 +37,7 @@ def normalization(Sxx):
          #Sxx_thisnorm = (Sxx[chan,:,j]-np.mean(Sxx[chan,:,:], axis = 1))/np.std(Sxx[chan,:,:], axis = 1)
          #Sxx_norm[:,j] = Sxx_thisnorm
       #Sxx_chan = np.expand_dims(Sxx_chan, axis = 0)
+      
       Sxx_norm[kl,:,:] = Sxx_chan
       ax[kl].pcolormesh(Sxx_chan, cmap = 'viridis')
       ax[kl].set_ylim(5,100)
