333d549b3b79fa4f6fd5e23946ee2aee8ea62ca8
BarbaraMath <mathiopoulou.barbara@gmail.com>
2023-09-26 17:06:28 +0200

    fooof

--- fooof_ftg_fun.py
+++ fooof_ftg_fun.py
@@ -6,11 +6,6 @@ from matplotlib import pyplot as plt
 
 
 def fit_fooof(SUBID, PATH, spectrum):
-    # Initialize a FOOOF object
-    fm = FOOOF(min_peak_height=1,
-        max_n_peaks=10,
-        #peak_threshold=10,
-        )
 
     # Set the frequency range to fit the model
     freqs = np.arange(1,127)
@@ -18,15 +13,20 @@ def fit_fooof(SUBID, PATH, spectrum):
 
     # Initialize a FOOOF object
     fm = FOOOF(
-        aperiodic_mode="fixed", # fitting without knee component
-        verbose=True,)
+        max_n_peaks=6, 
+        aperiodic_mode='knee')
 
     fm.fit(freqs, spectrum, freq_range)
-    
+
+    print(f'{SUBID}: Offset = {fm.aperiodic_params_[0]}, '
+        f'Knee = {fm.aperiodic_params_[1]}, '
+        f'Exponent = {fm.aperiodic_params_[2]}')
+
     # Plotting Periodic Components
     fig, ax = plt.subplots(1,1, figsize=[12,6])
     fm.plot(plot_peaks='dot-shade-outline', add_legend=True, ax=ax)
-
+    plt.title(f'Error: {np.round(fm.error_, decimals = 4)}, R-squared: {np.round(fm.r_squared_, decimals = 4)}')
+    
     plt.savefig(os.path.join(
         PATH, f'{SUBID}_FOOOF_FIT.jpg'),
         dpi=200
